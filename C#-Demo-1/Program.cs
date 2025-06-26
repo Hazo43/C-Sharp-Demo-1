4041176b0cce38c6c4d044dd46f1e24c93c66598
abdogaber10 <hazogaber10@gmail.com>
2025-06-26 22:45:07 +0300

    Video 2

--- C#-Demo-1/Program.cs
+++ C#-Demo-1/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Print();
+
+            int X = 10;
+            int Y = 0;
+            Console.WriteLine(X/Y);    //  Error 
+
+
+        }
+        public static void Print()
+        {
+            
         }
     }
 }
